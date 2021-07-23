17261324054c650559ab4940e7e0e966ebc232fe
mike.dev <menglu95120@gmail.com>
2021-07-23 19:26:49 +0100

    configure redux-persist

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ import TodoList from './components/TodoList';
 // redux-store
 import { Provider } from 'react-redux';
 import store from './store'
+// redux-persist
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistor = persistStore(store);
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="container">
-        <AddItem />
-        <TodoList />
-        <CompleteList />
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="container">
+          <AddItem />
+          <TodoList />
+          <CompleteList />
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,17 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import todos from "./todoReducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["todos"]
+};
+
+const rootReducer = combineReducers({
   todos
 });
+
+export default persistReducer(persistConfig, rootReducer);
